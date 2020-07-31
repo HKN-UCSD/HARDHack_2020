438698727ea2e742d8b88c7b6f863bfa086a923f
Thai <ttgilles@ucsd.edu>
2020-07-31 13:05:13 -0700

    Updated templates/2019.html so CSS can be loaded.
    In templates/2019.html, the relative path for css files used to be
    '../static/...'. Since Frozen-Flask puts the generated index.html
    file in the root folder of this repo, all those relative paths
    need to be './static/...' instead.

--- server.py
+++ server.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template, redirect, url_for
-from flask_flatpages import FlatPages
 from flask_frozen import Freezer
 import yaml
 
 app = Flask(__name__)
 app.config.from_pyfile('settings.py')
-pages = FlatPages(app)
 freezer = Freezer(app)
 
 with open('./data/PageData.yaml') as file:
